b1e7b1ad9a81ba938a52163922f44fb35500ccf6
V Regalite <hoang1998lqd@gmail.com>
2022-05-10 16:38:50 +0700

    add

---
+++ main.js
@@ -0,0 +1,7 @@
+// Bài 3
+function tinhToan() {
+    let a = document.getElementById("soa").value;
+    let b = document.getElementById("sob").value;
+    S = parseInt(a) * parseInt(b) ;
+    document.getElementById("tich").value= S;
+}
